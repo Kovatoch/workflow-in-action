272fd9e78577359367dfd7c0f21e4546d9096fd1
Mikael Koch Drewes <mikael.koch.drewes@regionh.dk>
2023-08-08 14:48:18 +0200

    adding step 4

--- amount_files_before.sh
+++ amount_files_before.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-BEFO=`ls /tmp/testdir/ | wc -l`
-echo "`date`Der er før oprydning oprettet $BEFO"  >> /tmp/test_of_cleanup_scripts.log
+BEFO=`ls testdir/ | wc -l`
+echo "`date`Der er før oprydning oprettet $BEFO" 
 
